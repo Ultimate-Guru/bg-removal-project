f8b900204501c1456544ff97f50100b4e6b346dc
Oluwatosin <tosindaniel360@gmail.com>
2025-06-30 16:47:32 +0100

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import BgSlider from '../components/BgSlider'
+import Testimonials from '../components/Testimonials'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Steps />
+      <BgSlider />
+      <Testimonials />
+    </div>
+  )
+}
+
+export default Home
