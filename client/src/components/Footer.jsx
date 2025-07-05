85fe56dbb88c8d2768a09cbe8e9905469c1ee03c
Oluwatosin <tosindaniel360@gmail.com>
2025-07-05 09:04:37 +0100

    Refactor Footer and Steps components for improved layout and content clarity

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,14 +8,11 @@ const Footer = () => {
             <NavLink to='/'>
                 <img src={assets.logo} alt="Logo" width={150} loading='lazy' />
             </NavLink>
-            <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>
-                All rights reserved. Copyright <a href="https://oluwatosindaniel.vercel.app" target='_blank'> @Oluwatosin </a>
-            </p>
 
-            <div className='flex gap-1'>
-                <img width={40} src={assets.facebook_icon} alt="Facebook" />
-                <img width={40} src={assets.twitter_icon} alt="Twitter" />
-                <img width={40} src={assets.google_plus_icon} alt="Google Plus" />
+            <div>
+                <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>
+                    All rights reserved. Copyright <a href="https://oluwatosindaniel.vercel.app" target='_blank'> @Oluwatosin </a>
+                </p>
             </div>
         </div>
     )
